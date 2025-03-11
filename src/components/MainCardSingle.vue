6524d5a9589c689820b8424e80f17536b9ab0657
Joseph Perez <josephp@southtexascollege.edu>
2025-03-11 10:43:51 -0500

    Card Components Working

---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import { faker } from '@faker-js/faker'
+    const firstName = faker.person.firstName()
+    const lastName = faker.person.lastName()
+    
+</script>
+
+<template>
+    {{ firstName }} {{ lastName }} <br/>
+
+</template>
